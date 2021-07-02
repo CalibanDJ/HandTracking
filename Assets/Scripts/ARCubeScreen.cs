3fa32c303c70c4f640f337e654d1904520814215
Julian DUPUIS <julian.dupuis@bordeaux-inp.fr>
2021-07-02 22:18:01 +0200

    Updating the menu and block texture changes

--- Assets/Scripts/ARCubeScreen.cs
+++ Assets/Scripts/ARCubeScreen.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class ARCubeScreen : MonoBehaviour
 {
     [SerializeField]
-    private Material[] arCubeMaterial;
-    
+    private Mesh[] arCubeMesh;
 
     private string handTag = "Player";
     private Renderer cubeRenderer;
+
+    private MeshFilter meshFilter;
 
 
     void Start()
@@ -20,6 +21,7 @@ public class ARCubeScreen : MonoBehaviour
     private void Initialize()
     {
         cubeRenderer = GetComponent<Renderer>();
+        meshFilter = this.GetComponent<MeshFilter>();
     }
 
     private void Update()
@@ -37,7 +39,17 @@ public class ARCubeScreen : MonoBehaviour
         if (other.gameObject.tag == handTag)
         {
             Debug.Log("Screened !!!!");
+            meshFilter.sharedMesh = arCubeMesh[1];
             this.GetComponent<Screenshot>().TakeAShot();
         }
+    }
+
+    /// <summary>
+    /// Change material when exit the cube
+    /// </summary>
+    /// <param name="other">The collider that exits</param>
+    private void OnTriggerExit(Collider other)
+    {
+        meshFilter.sharedMesh = arCubeMesh[0];
     }
 }
